8e843b257c01d6c42333577c0f02964eda8c69bf
sehun-kim <rogarithm@gmail.com>
2021-03-10 18:55:22 +0900

    refine projects for arrays ch p3-5

--- projects/arrays/notes.txt.pm
+++ projects/arrays/notes.txt.pm
@@ -4,15 +4,13 @@ p1 ok.
 
 p2 ok.
 
-p3
+p3 ok.
 
-p4: too complex. simplify later.
+p4 refine by making macro for array length
 
-? p5: how to compute monthly interest? and how to compute annual interest from monthly one? For now, it seems:
+p5 works, but need to check if it computes as I intended, and requires (simple) test.
 
-compute annual interest from monthly interest deduced from original annual interest -> convert original values to be added to new values to be added
-
-is this right?
+Is this right? 1 annually increased value = compound of 12 iterately increased value
 
 ? p6:
 - type of the array when it has chars, whitespace, comma as element
--- projects/arrays/p3.c
+++ projects/arrays/p3.c
@@ -1,39 +1,40 @@
-/* Checks numbers for repeated digits. Continues until the user enters zero */
-
-#include <stdbool.h>   /* C99 only */
+// Checks numbers for repeated digits. Continues until the user enters zero
+#include <stdbool.h>
 #include <stdio.h>
 
 int main(void)
 {
-		bool digit_seen[10] = {false};
-		int digit;
-		long n;
+	bool digit_seen[10] = {false};
+	int digit;
+	long n;
 
-		while (1) // don't use n as a condition. it'll affect other thing.
+	while (1) // don't use n as a condition. it'll affect other thing.
+	{
+		printf("Enter a number. To quit, enter 0: ");
+		scanf("%ld", &n);
+
+		if (n == 0)
+			break;
+		while (n > 0)
 		{
-				printf("Enter a number. To quit, enter 0: ");
-				scanf("%ld", &n);
-
-				if (n == 0)
-						break;
-				while (n > 0)
-				{
-						digit = n % 10;
-						if (digit_seen[digit])
-								break;
-						// if break executed, go out of while loop.
-						// if executed before n becomes zero, some digit is
-						// repetitive, so print "Repeated digit"
-						// otherwise, there's no repeat. so print "No repeated digit"
-						digit_seen[digit] = true;
-						n /= 10;
-				}
-
-				if (n > 0)
-						printf("Repeated digit\n");
-				else
-						printf("No repeated digit\n");
+			digit = n % 10;
+			if (digit_seen[digit])
+				break; // if break is executed, get out of this while loop.
+			/*
+			if break is executed before n becomes zero, a digit_seen[digit] is true for some digit. It means that the digit is repeated.
+			*/
+			digit_seen[digit] = true;
+			n /= 10;
+			/*
+			if break statement is not executed, there's no repeated digit in given number.
+			*/
 		}
 
-		return 0;
+		if (n > 0)
+			printf("Repeated digit\n");
+		else
+			printf("No repeated digit\n");
+	}
+
+	return 0;
 }
